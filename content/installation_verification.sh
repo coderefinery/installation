208c5bc948b04d0da7db872feb962a954cd762f8
samumantha <samantha.wittke@csc.fi>
2023-03-08 17:59:20 +0200

    notes on tools for different weeks added

--- content/installation_verification.sh
+++ content/installation_verification.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+echo "#######################################"
+echo "### For week one of the Coderefinery workshop you will need git installed and your ssh keys set up correctly:"
+
 # check if git is installed and with which version
 if which git 2>/dev/null >/dev/null; then
    echo "*** $(git --version) found, good to go"
@@ -8,6 +11,16 @@ if which git 2>/dev/null >/dev/null; then
    exit
 fi
 
+#check that github ssh is setup correctly
+if ssh git@github.com 2>&1 | grep -q 'successfully'; then
+    echo "*** ssh keys seem to be setup correctly, good to go"
+  else
+    echo "*** something seems to have gone wrong with your ssh keys for github 
+setup, please follow https://coderefinery.github.io/installation/ssh/#ssh and then try again"
+fi
+
+echo "#######################################"
+echo "### For week two of the Coderefinery workshop you will need a relatively new version of conda installed, as well as a coderefinery conda environment setup:" 
 #check if conda is installed and with which version
 if which conda 2>/dev/null >/dev/null; then
     echo "*** $(conda --version) found, please note that if your conda version is rather old (~ version 4.8.0 and older), please consider updating to avoid possible issues. Please follow https://coderefinery.github.io/installation/conda/#if-you-have-miniconda-or-anaconda-but-you-have-not-used-it-for-a-long-time and then try again, or continue with the older version on own risk." 
@@ -29,10 +42,4 @@ if conda env list | grep "coderefinery" 2>/dev/null >/dev/null; then
     fi
 fi
 
-#check that github ssh is setup correctly
-if ssh git@github.com 2>&1 | grep -q 'successfully'; then
-    echo "*** ssh keys seem to be setup correctly, good to go"
-  else
-    echo "*** something seems to have gone wrong with your ssh keys for github 
-setup, please follow https://coderefinery.github.io/installation/ssh/#ssh and then try again"
-fi
+
