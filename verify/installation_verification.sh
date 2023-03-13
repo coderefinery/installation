83c0a8f81d107172b88d3e8125f9ee351972f488
Radovan Bast <bast@users.noreply.github.com>
2023-03-13 11:02:00 +0100

    let's use only one script to not diverge

--- verify/installation_verification.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# run script for week1
-./installation_verification_week1.sh
-
-# run script for week2
-./installation_verification_week2.sh
-
-
--- verify/verify-installation.sh
+++ verify/verify-installation.sh
@@ -15,15 +15,15 @@ fi
 if ssh git@github.com 2>&1 | grep -q 'successfully'; then
     echo "*** ssh keys seem to be setup correctly, good to go"
   else
-    echo "*** something seems to have gone wrong with your ssh keys for github 
+    echo "*** something seems to have gone wrong with your ssh keys for github
 setup, please follow https://coderefinery.github.io/installation/ssh/#ssh and then try again"
 fi
 
 echo "#######################################"
-echo "### For week two of the Coderefinery workshop you will need a relatively new version of conda installed, as well as a coderefinery conda environment setup:" 
+echo "### For week two of the Coderefinery workshop you will need a relatively new version of conda installed, as well as a coderefinery conda environment setup:"
 #check if conda is installed and with which version
 if which conda 2>/dev/null >/dev/null; then
-    echo "*** $(conda --version) found, please note that if your conda version is rather old (~ version 4.8.0 and older), please consider updating to avoid possible issues. Please follow https://coderefinery.github.io/installation/conda/#if-you-have-miniconda-or-anaconda-but-you-have-not-used-it-for-a-long-time and then try again, or continue with the older version on own risk." 
+    echo "*** $(conda --version) found, please note that if your conda version is rather old (~ version 4.8.0 and older), please consider updating to avoid possible issues. Please follow https://coderefinery.github.io/installation/conda/#if-you-have-miniconda-or-anaconda-but-you-have-not-used-it-for-a-long-time and then try again, or continue with the older version on own risk."
     #check if coderefinery conda environment exists, also under different name
     if conda env list | grep "coderefinery" 2>/dev/null >/dev/null; then
 	echo "*** coderefinery conda environment found, good to go"
